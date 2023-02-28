ad544cc201a6e309f870d79d992c2723574fec4b
Ruskin Acevedo <ruskin.acevedo@gmail.com>
2023-02-28 15:52:14 -0500

    made comments in JS

--- assets/script.js
+++ assets/script.js
@@ -5,13 +5,23 @@ $(function () {
     showSavedTasks()
 });
 
+// Handles displaying the current day
+var timeDisplayEl = $('#currentDay');
+function displayTime() {
+    var rightNow = dayjs().format('dddd, MMM D, YYYY hh:mm A');
+    timeDisplayEl.text(rightNow);
+}
+
+//  Get current hours
 var timeNow = dayjs().hour();
 function addTimeClasses() {
+    //    Loop over time blocks and check current
     for (var hour = 9; hour < 18; hour++) {
         var timeBlock = $(`#hour-${hour}`)
         timeBlock.removeClass("past present future")
+        // checks for time and if we have moved past this time
         if (hour === timeNow) {
-            timeBlock.addClass('present')
+            timeBlock.addClass("present")
 
         } else if (hour < timeNow) {
             timeBlock.addClass("past")
@@ -24,22 +34,24 @@ function addTimeClasses() {
     }
 }
 
-// event listener for when save button is clicked
+
 var saveBtn = $('.saveBtn');
 
-
+// event listener for when save button is clicked
 saveBtn.on('click', function () {
     var textArea = $(this).siblings("textarea");
     var value = textArea.val();
     var key = `hour-${textArea.data("hour")}`;
-
+    // Save in local storage
     localStorage.setItem(key, value);
+    ;
+
 
     showSavedTasks()
 
-
 });
 
+// loads data saved in local storage and shows it in textarea
 function showSavedTasks() {
     for (var hour = 9; hour < 18; hour++) {
         var key = `hour-${hour}`
@@ -50,11 +62,3 @@ function showSavedTasks() {
     }
 
 }
-
-// Handles displaying the current day
-var timeDisplayEl = $('#currentDay');
-function displayTime() {
-    var rightNow = dayjs().format('dddd, MMM D, YYYY hh:mm A ');
-    timeDisplayEl.text(rightNow);
-}
-
